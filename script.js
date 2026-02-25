2f18c32d7459fc5bb116ebb183be96faee6792a5
KevinGTX <A20050001@UNISIERRA.EDU.MX>
2026-02-25 14:42:34 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,347 @@
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let monsterHealth;
+let inventory = ["palo"];
+
+const button1 = document.querySelector('#button1');
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
+const monsterImage = document.querySelector("#monsterImage");
+
+const weapons = [
+  { name: 'palo', power: 5 },
+  { name: 'machete oxidado', power: 20 },
+  { name: 'daga', power: 30 },
+  { name: 'martillo de orejas', power: 50 },
+  { name: 'espada', power: 100 },
+  { name: 'dragon slayer', power: 250 },
+  { name: 'staff de polvo estelar', power: 500 }
+];
+
+const monsters = [
+  { 
+    name: "slime", 
+    level: 2, 
+    health: 15, 
+    image: "fotos/slime.png" 
+  },
+  { 
+    name: "esqueleto", 
+    level: 5, 
+    health: 40, 
+    image: "fotos/esqueleto.png" 
+  },
+  { 
+    name: "bestia colmilluda", 
+    level: 8, 
+    health: 60, 
+    image: "fotos/bestia.png" 
+  },
+  { 
+    name: "arbol maldito", 
+    level: 12, 
+    health: 120, 
+    image: "fotos/arbol_maldito.png"
+  },
+  { 
+    name: "dragon", 
+    level: 20, 
+    health: 300, 
+    image: "fotos/dragon.png" 
+  }
+];
+
+ const locations = [
+ {
+    name: "plaza del pueblo",
+    "button text": ["Ir a la tienda", "Ir a la cueva", "Luchar con dragon"],
+    "button functions": [goStore, goCave, fightDragon],
+    text: "Estas en la plaza del pueblo. Ves un letrero que dice \"Tienda\".",
+    background: "url('fotos/town.png')" 
+  },
+  {
+    name: "tienda",
+    "button text": ["Comprar 10 salud (10 oro)", "Comprar arma (30 oro)", "Ir a la plaza"],
+    "button functions": [buyHealth, buyWeapon, goTown],
+    text: "Entras a la tienda.",
+    background: "url('fotos/store.png')" 
+  },
+  {
+    name: "cueva",
+    "button text": ["Luchar con monstruos", "Luchar con arbol", "Ir a la plaza"],
+    "button functions": [goCaveMonsters, fightTree, goTown],
+    text: "Entras a la cueva. Ves algunos monstruos oscuros.",
+    background: "url('fotos/monster_cave.png')" 
+  },
+  {
+    name: "lucha",
+    "button text": ["Atacar", "Esquivar", "Correr"],
+    "button functions": [attack, dodge, goTown],
+    text: "Estas luchando con un monstruo."
+  },
+  {
+    name: "matar monstruo",
+    "button text": ["Ir a la plaza", "Ir a la plaza", "Ir a la plaza"],
+    "button functions": [goTown, goTown, goTown],
+    text: 'El monstruo grita "Arg!" mientras muere. Ganas puntos de experiencia y encuentras oro.'
+  },
+  {
+    name: "perder",
+    "button text": ["REPETIR?", "REPETIR?", "REPETIR?"],
+    "button functions": [restart, restart, restart],
+    text: "Has muerto. &#x2620;"
+  },
+  { 
+    name: "ganar", 
+    "button text": ["REPETIR?", "REPETIR?", "REPETIR?"], 
+    "button functions": [restart, restart, restart], 
+    text: "Derrotaste al dragon! HAS GANADO EL JUEGO! &#x1F389;" 
+  },
+  {
+    name: "huevo de pascua",
+    "button text": ["2", "8", "Ir a la plaza"],
+    "button functions": [pickTwo, pickEight, goTown],
+    text: "Encontraste un juego secreto. Elige un numero arriba. Se elegiran diez numeros al azar entre 0 y 10. Si el numero que eliges coincide con uno de los numeros al azar, ganas!"
+  }
+];
+
+// initialize buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
+function update(location) {
+  monsterStats.style.display = "none";
+  button1.innerText = location["button text"][0];
+  button2.innerText = location["button text"][1];
+  button3.innerText = location["button text"][2];
+  button1.onclick = location["button functions"][0];
+  button2.onclick = location["button functions"][1];
+  button3.onclick = location["button functions"][2];
+  text.innerHTML = location.text;
+  
+  if(location.background) {
+    document.body.style.backgroundImage = location.background;
+    document.body.style.backgroundSize = "cover";
+  }
+}
+
+function goTown() {
+  update(locations[0]);
+}
+
+function goStore() {
+  update(locations[1]);
+}
+
+function goCave() {
+  update(locations[2]);
+}
+
+function buyHealth() {
+  if (gold >= 10) {
+    gold -= 10;
+    health += 10;
+    goldText.innerText = gold;
+    healthText.innerText = health;
+  } else {
+    text.innerText = "No tienes suficiente oro para comprar salud.";
+  }
+}
+
+function buyWeapon() {
+  if (currentWeapon < weapons.length - 1) {
+    if (gold >= 30) {
+      gold -= 30;
+      currentWeapon++;
+      goldText.innerText = gold;
+      let newWeapon = weapons[currentWeapon].name;
+      text.innerText = "Ahora tienes: " + newWeapon + ".";
+      inventory.push(newWeapon);
+      text.innerText += " En tu inventario tienes: " + inventory;
+    } else {
+      text.innerText = "No tienes suficiente oro para comprar un arma.";
+    }
+  } else {
+    text.innerText = "Ya tienes el arma mas poderosa: el Staff de Polvo Estelar!";
+    button2.innerText = "Vender arma por 15 oro";
+    button2.onclick = sellWeapon;
+  }
+}
+
+function sellWeapon() {
+  if (inventory.length > 1) {
+    gold += 15;
+    goldText.innerText = gold;
+    let currentWeapon = inventory.shift();
+    text.innerText = "Vendiste una " + currentWeapon + ".";
+    text.innerText += " En tu inventario tienes: " + inventory;
+  } else {
+    text.innerText = "¡No vendas tu unica arma!";
+  }
+}
+function goCaveMonsters() {
+  let azar = Math.random();
+  
+  if (azar < 0.33) {
+    fighting = 0; 
+  } else if (azar < 0.66) {
+    fighting = 1; 
+  } else {
+    fighting = 2; 
+  }
+  
+  goFight();
+}
+function fightSkeleton() {
+  fighting = 1; 
+  goFight();
+}
+
+function fightTree() {
+  fighting = 3; 
+  goFight();
+}
+function fightSlime() {
+  fighting = 0;
+  goFight();
+}
+
+function fightBeast() {
+  fighting = 2;
+  goFight();
+}
+
+function fightDragon() {
+  fighting = 4;
+  goFight();
+}
+
+function goFight() {
+  update(locations[3]);
+  monsterHealth = monsters[fighting].health;
+  monsterStats.style.display = "block";
+  monsterName.innerText = monsters[fighting].name;
+  monsterHealthText.innerText = monsterHealth;
+  
+  monsterImage.src = monsters[fighting].image;
+}
+
+function attack() {
+  text.innerText = "El " + monsters[fighting].name + " ataca.";
+  text.innerText += " Lo etiquetas con tu " + weapons[currentWeapon].name + ".";
+  health -= getMonsterAttackValue(monsters[fighting].level);
+  
+  if (isMonsterHit()) {
+    monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;    
+  } else {
+    text.innerText += " Fallaste.";
+  }
+  
+  healthText.innerText = health;
+  monsterHealthText.innerText = monsterHealth;
+  
+  if (health <= 0) {
+    lose();
+  } else if (monsterHealth <= 0) {
+    
+    if (fighting === 4) { 
+      winGame();
+    } else {
+      defeatMonster();
+    }
+  }
+  
+  // Lógica de romper arma (opcional)
+  if (Math.random() <= .1 && inventory.length !== 1) {
+    text.innerText += " Tu " + inventory.pop() + " se rompe.";
+    currentWeapon--;
+  }
+}
+
+function getMonsterAttackValue(level) {
+  const hit = (level * 5) - (Math.floor(Math.random() * xp));
+  console.log(hit);
+  return hit > 0 ? hit : 0;
+}
+
+function isMonsterHit() {
+  return Math.random() > .2 || health < 20;
+}
+
+function dodge() {
+  text.innerText = "Esquivas el ataque del " + monsters[fighting].name;
+}
+
+function defeatMonster() {
+  gold += Math.floor(monsters[fighting].level * 6.7);
+  xp += monsters[fighting].level;
+  goldText.innerText = gold;
+  xpText.innerText = xp;
+  update(locations[4]);
+}
+
+function lose() {
+  update(locations[5]);
+}
+
+function winGame() {
+  update(locations[6]);
+}
+
+function restart() {
+  xp = 0;
+  health = 100;
+  gold = 50;
+  currentWeapon = 0;
+  inventory = ["palo"];
+  goldText.innerText = gold;
+  healthText.innerText = health;
+  xpText.innerText = xp;
+  goTown();
+}
+
+function easterEgg() {
+  update(locations[7]);
+}
+
+function pickTwo() {
+  pick(2);
+}
+
+function pickEight() {
+  pick(8);
+}
+
+function pick(guess) {
+  const numbers = [];
+  while (numbers.length < 10) {
+    numbers.push(Math.floor(Math.random() * 11));
+  }
+  text.innerText = "Elegiste " + guess + ". Aqui están los numeros aleatorios:\n";
+  for (let i = 0; i < 10; i++) {
+    text.innerText += numbers[i] + "\n";
+  }
+  if (numbers.includes(guess)) {
+    text.innerText += "¡Correcto! ¡Ganas 20 de oro!";
+    gold += 20;
+    goldText.innerText = gold;
+  } else {
+    text.innerText += "¡Incorrecto! ¡Pierdes 10 de salud!";
+    health -= 10;
+    healthText.innerText = health;
+    if (health <= 0) {
+      lose();
+    }
+  }
+}
